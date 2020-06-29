ab3f07dbc9ae26187303c53b4beb91cec3e882fd
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-29 06:36:41 +0000

    adding_file

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The console: contains the entry point of the command interpreter
+BETA VERSION The console: contains the entry point of the command interpreter
 """
  
 import cmd
