2d63b0db6c71a5c922d5c83484319ac930d448c7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-30 07:01:49 +0000

    adding_file

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,13 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
             models.storage.save()
 
     def __str__(self):
         """string readable representation of the instance"""
-        return "[{}] ({}) {}".format(self.getclsname(), self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute with the current datetime"""
