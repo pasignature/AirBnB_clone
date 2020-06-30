62056840d8fa1a9b06f57510d717cc1f3522b68a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-30 15:43:30 +0000

    adding_file

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            #models.storage.save()
+            models.storage.save()
 
     def __str__(self):
         """string readable representation of the instance"""
