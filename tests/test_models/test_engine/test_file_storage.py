fa7eeb5f3e06876173c8c7f39b09693bf5c5ba92
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-01 20:54:27 +0000

    adding_fiel

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,6 +15,34 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+class TestFileStorage_methods(unittest.TestCase):
+    """Unittests for testing methods of the FileStorage class."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_all_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
 
 class TestFileStorage_instantiation(unittest.TestCase):
     """Test mainly focused on the instansiation of class"""
