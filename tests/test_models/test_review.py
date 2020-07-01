70db976c85fd073ea9d17eddc18c5c7497b59312
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-01 19:56:10 +0000

    adding_file

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -12,8 +12,19 @@ from models.review import Review
 class TestReview_instantiation(unittest.TestCase):
     """testing classinstantiation the class."""
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(Review, type(Review()))
+    def test_subclass(self):
+        self.assertNotEqual(Review, type(Review()))
+
+    def test_ids_generation(self):
+        instance1 = Review()
+        instance2 = Review()
+        self.assertNotEqual(instance1.id, instance2.id)
+
+    def test_attribute_object_time(self):
+        self.assertEqual(datetime, type(Review().updated_at))
+
+    def test_attribute2_datetime(self):
+        self.assertEqual(datetime, type(Review().created_at))
 
 if __name__ == "__main__":
     unittest.main()
