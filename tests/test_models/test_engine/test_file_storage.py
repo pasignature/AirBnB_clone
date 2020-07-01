9ecdee7ce8d67308b56e7554634f2282c6c0b98f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-01 20:34:54 +0000

    adding_file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,6 +8,7 @@ import inspect
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+import models
 import json
 import os
 import pep8
