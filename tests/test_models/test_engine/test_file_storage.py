d1c12e55e1005f6fceedbfde81cd300b21f9c14e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-01 20:47:22 +0000

    adding_file

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Unitestets file for the file_storage implementatin
+"""
+import os
+import json
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Test mainly focused on the instansiation of class"""
+
+    def test_FileStorage_standard_instantiation(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorage_instnatiation_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_FileStorage_Privacy_file_path(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testFileStorage_provacy_obj_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializes(self):
+        self.assertEqual(type(models.storage), FileStorage)
+
+
